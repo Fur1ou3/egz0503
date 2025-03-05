9318d314abf40a34c3ed31bac868d77eb9bdeee6
Fur1ou3 <wpmemiszta9@gmail.com>
2025-03-05 13:00:51 +0100

    css+php

---
+++ organizer.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Organizer</title>
+    <link rel="stylesheet" href="styl6.css">
+</head>
+<body>
+    <div id="baner1">
+         <h2>MÓJ ORGANIZER</h2>
+    </div>
+
+    <div id="baner2">
+        <form action="organizer.php" method="post">
+            <label for="wydarzenie">Wpis wydarzenia:</label> <input type="text" name="wydarzenie" id="wydarzenie"> <button type="submit">ZAPISZ</button>
+        </form>
+    </div>
+
+    <div id="baner3">
+        <img src="logo2.png" alt="Mój organizer">
+    </div>
+    <main>
+    
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
